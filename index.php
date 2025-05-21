a8a9e6b0df6f64c0c13872d9094341fd19a836e9
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2025-05-21 03:36:33 -0300

    Update index.php
    Remove restrição de acesso

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-session_start();
-$usuarios_pendentes = "/var/www/restricted/pendentes.txt";
+//session_start();
+//$usuarios_pendentes = "/var/www/restricted/pendentes.txt";
 
 // Verifica se o servidor passou a variável de autenticação
-if (!isset($_SERVER['PHP_AUTH_USER'])) {
-    header('WWW-Authenticate: Basic realm="Restricted"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo "Acesso negado!";
-    exit;
-}
-$usuario = $_SERVER['PHP_AUTH_USER'];
+//if (!isset($_SERVER['PHP_AUTH_USER'])) {
+//    header('WWW-Authenticate: Basic realm="Restricted"');
+//    header('HTTP/1.0 401 Unauthorized');
+//    echo "Acesso negado!";
+//    exit;
+//}
+//$usuario = $_SERVER['PHP_AUTH_USER'];
 
 // Verifica se o usuário está na lista de troca de senha
-$usuarios_pendentes_lista = file($usuarios_pendentes, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-if (in_array($usuario, $usuarios_pendentes_lista)) {
-    header("Location: /trocar_senha.php");
-    exit;
-}
+//$usuarios_pendentes_lista = file($usuarios_pendentes, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+//if (in_array($usuario, $usuarios_pendentes_lista)) {
+//    header("Location: /trocar_senha.php");
+//    exit;
+//}
 
 if (file_exists("./pgs/functions.php")) {
     require_once("./pgs/functions.php");
@@ -220,7 +220,7 @@ if (!$isAjax) {
             <br />Refletor D-Star Multiprotocolo <b><?php echo $Reflector->GetReflectorName(); ?></b> v<?php echo $Reflector->GetVersion();?> - Dashboard v<?php echo $PageOptions['DashboardVersion']; ?>
             | Personalizado por Daniel K. <b><a href="https://www.qrz.com/db/PU5KOD">PU5KOD</a> (<a href="https://t.me/PU5KOD">Telegram</a> / <a href="https://api.whatsapp.com/send?phone=5541991912000">WhatsApp</a>)</b>
             <br />Tempo em serviço: <span id="suptime"><?php echo FormatSeconds($Reflector->GetServiceUptime());?></span>
-            <?php echo '<p><a href="http://cloud.dvbr.net"><center><img src="./img/SaveInCloud_white.svg" width="125"></center></a></p>';?>
+            <?php echo '<p><a href="http://cloud.dvbr.net"><center><img src="./img/powered-by-aws-white.png" width="125"></center></a></p>';?>
         </div>
     </div>
 <?php
