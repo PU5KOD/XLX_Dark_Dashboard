72057f08077e8ce8cc94f7205c9a00a51d85043a
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2025-05-23 11:05:45 -0300

    Update repeaters_no-band-col.php

--- pgs/repeaters_no-band-col.php
+++ pgs/repeaters_no-band-col.php
@@ -156,11 +156,11 @@ for ($i=0;$i<$Reflector->NodeCount();$i++) {
         }
         echo '</td>
             <td align="center">';
-        $callsignCompleto = $Reflector->Nodes[$i]->GetCallSign();
-        $sufixo = $Reflector->Nodes[$i]->GetSuffix();
-        echo '<a href="https://www.qrz.com/db/'.$callsignCompleto.'" class="pl" title="Clique aqui para consultar o QRZ da estação" target="_blank">'.$callsignCompleto.'</a>';
-        if ($sufixo) {
-            echo ' - <b>'.$sufixo.'</b>';
+        $FullCallsign = $Reflector->Nodes[$i]->GetCallSign();
+        $Suffix = $Reflector->Nodes[$i]->GetSuffix();
+        echo '<a href="https://www.qrz.com/db/'.$FullCallsign.'" class="pl" title="Clique aqui para consultar o QRZ da estação" target="_blank">'.$FullCallsign.'</a>';
+        if ($Suffix) {
+            echo ' - <b>'.$Suffix.'</b>';
         }
         echo '</td>
             <td align="center">'.date("d/m/Y, H:i:s", $Reflector->Nodes[$i]->GetLastHeardTime()).'</td>
