7b9fac5d276c8dce1d168e6ec344778f840fbd3b
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2025-05-30 10:09:00 -0300

    Update update_db.sh

--- users_db/update_db.sh
+++ users_db/update_db.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 echo ""
-echo "Atualizando arquivo da Base de Dados"
+echo "Updating Database file"
 echo ""
-wget -O /xlxd/users_db/user.csv https://radioid.net/static/user.csv
-echo "Compilando Banco de Dados"
-echo ""
+FILE_SIZE=$(wget --spider --server-response https://radioid.net/static/user.csv 2>&1 | grep -i Content-Length | awk '{print $2}')
+if [ -z "$FILE_SIZE" ]; then
+    echo "Downloading..."
+    wget -q -O /xlxd/users_db/user.csv https://radioid.net/static/user.csv
+else
+    wget -q -O - https://radioid.net/static/user.csv | pv -s "$FILE_SIZE" > /xlxd/users_db/user.csv
+fi
+echo "Compiling Database"
 php /xlxd/users_db/create_user_db.php
-echo ""
-echo "Base e Banco de Dados atualizados com sucesso!"
+echo "Database and Base updated successfully!"
 echo ""
