43b9aae0247c5d2ed30644657597332bbf2bdde7
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2025-06-19 03:34:28 -0300

    Update traffic.php
    English translate

--- pgs/traffic.php
+++ pgs/traffic.php
@@ -1,12 +1,12 @@
 <?php
-// Habilitar log de erros
+// Enable error logging
 //ini_set('log_errors', 1);
 //ini_set('error_log', '/var/www/html/xlxd/pgs/php_errors.log');
-// Verificar se $VNStat está definido
+// Check if $VNStat is set
 if (!isset($VNStat)) {
-    die("Erro: \$VNStat não está definido");
+    die("Error: \$VNStat isn't defined");
 }
-// Validação da interface
+// Interface validation
 if (!isset($_GET['iface'])) {
     if (isset($VNStat['Interfaces'][0]['Address'])) {
         $_GET['iface'] = $VNStat['Interfaces'][0]['Address'];
@@ -26,28 +26,28 @@ if (!isset($_GET['iface'])) {
         $_GET['iface'] = "";
     }
 }
-// Definir locale para português
-setlocale(LC_TIME, 'pt_BR.UTF-8', 'pt_BR', 'portuguese');
+// Set locale to English
+setlocale(LC_TIME, 'en.UTF-8', 'en', 'english');
 ?>
 <table class="listingtable" style="padding: 0; margin: 0 auto; width: 900px;">
  <tr>
-   <th>Estatísticas da Interface de Rede</th>
+   <th>Network Interface Statistics</th>
  </tr>
  <tr>
     <td bgcolor="#2c2c2c" style="padding: 10px;">
     <?php
     $Data = VNStatGetData($_GET['iface'], $VNStat['Binary']);
-    // Estilo para as tabelas
+    // Style for tables
     $tableStyle = 'border-collapse: collapse; margin: 10px auto; width: 100%;';
     $thStyle = 'background-color: #3a3a3a; color: #c3dcba; font-weight: bold; padding: 1px; border: 1px solid #606060; text-align: center; white-space: nowrap;';
     $tdStyle = 'padding: 3px; border: 1px solid #606060; text-align: center; white-space: nowrap; color: #c3dcba;';
-    // Exibir totais gerais
+    // Display grand totals
     echo '
     <table style="' . $tableStyle . '">
         <tr>
             <th style="' . $thStyle . '">Total RX</th>
             <th style="' . $thStyle . '">Total TX</th>
-            <th style="' . $thStyle . '">Total Geral (TX + RX)</th>
+            <th style="' . $thStyle . '">Grand total (TX + RX)</th>
         </tr>
         <tr>
             <td style="' . $tdStyle . '">' . format_traffic($Data['totals']['rx'], $Data['totals']['rx_unit']) . '</td>
@@ -55,25 +55,25 @@ setlocale(LC_TIME, 'pt_BR.UTF-8', 'pt_BR', 'portuguese');
             <td style="' . $tdStyle . '">' . format_traffic($Data['totals']['total'], $Data['totals']['total_unit']) . '</td>
         </tr>
     </table>';
-    // Exibir dados mensais
+    // Display monthly data
     echo '
     <table style="' . $tableStyle . '">
         <tr>
-            <th style="' . $thStyle . '" colspan="5">Dados Mensais</th>
-            <th style="' . $thStyle . '" colspan="3">Valores Estimados</th>
+            <th style="' . $thStyle . '" colspan="5">Monthly Data</th>
+            <th style="' . $thStyle . '" colspan="3">Estimated Values</th>
         </tr>
         <tr>
-            <th style="' . $thStyle . '">Mês</th>
+            <th style="' . $thStyle . '">Month</th>
             <th style="' . $thStyle . '">RX</th>
             <th style="' . $thStyle . '">TX</th>
             <th style="' . $thStyle . '">Total</th>
-            <th style="' . $thStyle . '">Vel. Média</th>
+            <th style="' . $thStyle . '">Average Rate</th>
             <th style="' . $thStyle . '">RX</th>
             <th style="' . $thStyle . '">TX</th>
             <th style="' . $thStyle . '">Total</th>
         </tr>';
     if (empty($Data['monthly'])) {
-        echo "<tr><td colspan='8' style='$tdStyle'>Nenhum dado mensal disponível</td></tr>";
+        echo "<tr><td colspan='8' style='$tdStyle'>No monthly data available</td></tr>";
     }
     foreach ($Data['monthly'] as $month) {
         if (isset($month['time']) && $month['time'] > 0) {
@@ -89,29 +89,29 @@ setlocale(LC_TIME, 'pt_BR.UTF-8', 'pt_BR', 'portuguese');
                 <td style="' . $tdStyle . '">' . (isset($month['estimated']['total']) ? format_traffic($month['estimated']['total'], $month['estimated']['total_unit']) : '-') . '</td>
             </tr>';
         } else {
-            echo "<tr><td colspan='8' style='$tdStyle'>Dado mensal sem timestamp válido</td></tr>";
+            echo "<tr><td colspan='8' style='$tdStyle'>Monthly data without valid timestamp</td></tr>";
         }
     }
     echo '</table>';
-    // Exibir dados diários
+    // Display daily data
     echo '
     <table style="' . $tableStyle . '">
         <tr>
-            <th style="' . $thStyle . '" colspan="5">Dados Diários</th>
-            <th style="' . $thStyle . '" colspan="3">Valores Estimados</th>
+            <th style="' . $thStyle . '" colspan="5">Daily Data</th>
+            <th style="' . $thStyle . '" colspan="3">Estimated Values</th>
         </tr>
         <tr>
-            <th style="' . $thStyle . '">Dia</th>
+            <th style="' . $thStyle . '">Day</th>
             <th style="' . $thStyle . '">RX</th>
             <th style="' . $thStyle . '">TX</th>
             <th style="' . $thStyle . '">Total</th>
-            <th style="' . $thStyle . '">Vel. Média</th>
+            <th style="' . $thStyle . '">Average Rate</th>
             <th style="' . $thStyle . '">RX</th>
             <th style="' . $thStyle . '">TX</th>
             <th style="' . $thStyle . '">Total</th>
         </tr>';
     if (empty($Data['daily'])) {
-        echo "<tr><td colspan='8' style='$tdStyle'>Nenhum dado diário disponível</td></tr>";
+        echo "<tr><td colspan='8' style='$tdStyle'>No daily data available</td></tr>";
     }
     foreach ($Data['daily'] as $day) {
         if (isset($day['time']) && $day['time'] > 0) {
@@ -127,25 +127,25 @@ setlocale(LC_TIME, 'pt_BR.UTF-8', 'pt_BR', 'portuguese');
                 <td style="' . $tdStyle . '">' . (isset($day['estimated']['total']) ? format_traffic($day['estimated']['total'], $day['estimated']['total_unit']) : '-') . '</td>
             </tr>';
         } else {
-            echo "<tr><td colspan='8' style='$tdStyle'>Dado diário sem timestamp válido</td></tr>";
+            echo "<tr><td colspan='8' style='$tdStyle'>Daily data without valid timestamp</td></tr>";
         }
     }
     echo '</table>';
-    // Exibir "Database updated" e "since"
+    // Display "Database updated" and "since"
     echo '<div style="margin: 10px; text-align: right; font-size: 12px; color: #c3dcba;">';
-    // Ajustar formato da data de atualização do banco de dados
+    // Adjust database update date format
     $databaseUpdated = DateTime::createFromFormat('Y-m-d H:i:s', $Data['database_updated']);
     if ($databaseUpdated) {
-        echo "Última atualização: " . $databaseUpdated->format('d/m/Y, H:i:s') . "<br>";
+        echo "Last update: " . $databaseUpdated->format('d/m/Y, H:i:s') . "<br>";
     } else {
-        echo "Última atualização: " . htmlspecialchars($Data['database_updated']) . "<br>";
+        echo "Last update: " . htmlspecialchars($Data['database_updated']) . "<br>";
     }
-    // Ajustar formato da data "since"
+    // Adjust "since" date format
     $sinceDate = DateTime::createFromFormat('Y-m-d', $Data['since']);
     if ($sinceDate) {
-        echo "Interface " . htmlspecialchars($_GET['iface']) . " desde " . $sinceDate->format('d/m/Y') . "<br>";
+        echo "Interface " . htmlspecialchars($_GET['iface']) . " since " . $sinceDate->format('d/m/Y') . "<br>";
     } else {
-        echo "Interface " . htmlspecialchars($_GET['iface']) . " desde " . htmlspecialchars($Data['since']) . "<br>";
+        echo "Interface " . htmlspecialchars($_GET['iface']) . " since " . htmlspecialchars($Data['since']) . "<br>";
     }
     echo '</div>';
     ?>
