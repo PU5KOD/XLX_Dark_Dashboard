e6fd955186d1dceb3f74dc9a18d4a453469577e5
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2025-08-12 15:21:02 -0300

    Update index.php

--- log/index.php
+++ log/index.php
@@ -46,9 +46,9 @@ if (isset($_SESSION['password'])) {
 <head>
    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
    <title>XLX Live Log Monitor</title>
-<!-- Fontes monoespacadas do Google Fonts -->
+<!-- Google Monospace Fonts -->
    <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Fira+Code&family=Source+Code+Pro&family=Roboto+Mono&display=swap" rel="stylesheet">
-<!-- Fonte Awesome -->
+<!-- Awesome Font -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
    <style>
       body {
