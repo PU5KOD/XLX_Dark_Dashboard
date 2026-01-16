39629df8896b01959484a8e349feaa4fc6da56c4
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2026-01-16 06:25:17 -0300

    Refactor HTML structure for live IRC display

--- pgs/liveircddb.php
+++ pgs/liveircddb.php
@@ -1,4 +1,9 @@
-<div style="text-align: center; width: 90%; background-color: #1a1a1a; padding: 10px;">
-   <iframe width="1050" height="800" src="./pgs/ircddb_proxy.php" style="border: 2px #444444 solid; margin-top:30px; background-color:#909090;" align="center"></iframe>
-   <iframe width="875" height="770" src="https://irc-1.openquad.net/ics/" style="border: 2px solid #444444; margin-top: 20px; background-color: #909090;" align="center"></iframe>
+<h3 style="text-align: center; color: #f0f075; background-color: #1a1a1a; padding: 10px; margin-bottom: 0; border-radius: 5px 5px 0 0; font-family: Arial, sans-serif; font-size: 1.5em;">ircDDB Live</h3h3>
+<div style="text-align: center; width: 100%; background-color: #1a1a1a; padding: 10px; border-radius: 0 0 5px 5px;">
+   <iframe width="1050" height="800" src="./pgs/ircddb_proxy.php" style="border: 2px #444444 solid; margin-top: 1px; background-color:#eeeeec;" align="center"></iframe>
 </div>
+
+<h3 style="text-align: center; color: #f0f075; background-color: #1a1a1a; padding: 10px; margin-bottom: 0; border-radius: 5px 5px 0 0; font-family: Arial, sans-serif; font-size: 1.5em;">QuadNet Live</h3>
+<div style="text-align: center; width: 100%; background-color: #1a1a1a; padding: 10px; border-radius: 0 0 5px 5px;">
+   <iframe width="875" height="770" src="https://irc-1.openquad.net/ics/" style="border: 2px solid #444444; margin-top: 1px; background-color: #909090;" align="center"></iframe>
+div</div>
