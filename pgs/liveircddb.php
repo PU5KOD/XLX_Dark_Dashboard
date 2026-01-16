3a38a677f6cab38d13ccb6d10c4ad7fa20bdd219
Daniel Kondlatsch <daniel.kondlatsch@gmail.com>
2026-01-15 22:25:53 -0300

    Update iframe dimensions and source in liveircddb.php

--- pgs/liveircddb.php
+++ pgs/liveircddb.php
@@ -1,3 +1,4 @@
 <div style="text-align: center; width: 90%; background-color: #1a1a1a; padding: 10px;">
-   <iframe width="874" height="770" src="https://irc-1.openquad.net/ics/" style="border: 2px solid #444444; margin-top: 20px; background-color: #909090;" align="center"></iframe>
+   <iframe width="1050" height="800" src="./pgs/ircddb_proxy.php" style="border: 2px #444444 solid; margin-top:30px; background-color:#909090;" align="center"></iframe>
+   <iframe width="875" height="770" src="https://irc-1.openquad.net/ics/" style="border: 2px solid #444444; margin-top: 20px; background-color: #909090;" align="center"></iframe>
 </div>
